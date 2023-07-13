3613d3f92b371efbda4132c57d470ea4d211cb72
elyton250 <nelysemathieu@gmail.com>
2023-07-13 11:10:16 -0700

    push

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(void)
+	/**
+	 * main -entry point
+	 * retorn -sucess -0
+	 */
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  *Retutrn: always 0 (success)
+  */
+int main(void)
+/*main - Entry point*/
+{
+	printf("size of a char: %lu byte(s)\n", sizeof(char));
+	printf("size of a int: %lu bytes(s)\n", sizeof(int));
+	printf("size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("size of a float:%lu byte(s)\n", sizeof(float));
+	return (0);
+}
