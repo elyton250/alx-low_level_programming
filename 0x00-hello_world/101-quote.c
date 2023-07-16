9c60b99d7916a68ad39d1b09fd2200b6c6248306
elyton250 <nelysemathieu@gmail.com>
2023-07-16 09:14:26 -0700

    add tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+  *main -entrypoint
+  *
+  *Description:prints with error
+  *
+  *Return: 1 (sucess)
+  */
+int main()
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
