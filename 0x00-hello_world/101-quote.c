817976c4072fce25813c49ba9eb63584a52bbd33
elyton250 <nelysemathieu@gmail.com>
2023-07-16 09:33:08 -0700

    12

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-  *main -entrypoint
-  *
-  *Description:prints with error
-  *
-  *Return: 1 (sucess)
-  */
-int main()
+ * main - Entry point of the program
+ *
+ * Description: Prints a message to the standard error stream
+ *
+ * Return: Always 1 (success)
+ */
+int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf(stderr, "and that piece of art is useful\"
+		    - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
