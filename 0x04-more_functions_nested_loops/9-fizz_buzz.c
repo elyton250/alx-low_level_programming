8e77db865746b56bb3adc95a1163705a9529e089
elyton250 <nelysemathieu@gmail.com>
2023-07-20 06:37:02 -0700

    first patch

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  *main - entry point
+  *
+  *Return: always 0
+  *
+  */
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 3 == 0 && n % 5 != 0)
+		{
+			printf("Fizz");
+		}
+		else if (n % 5 == 0 && n % 3 != 0)
+		{
+			printf("Buzz");
+		}
+		else if (n % 5 == 0 && n % 3 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", n);
+		}
+		printf("\n");
+	}
+	return (0);
+}
