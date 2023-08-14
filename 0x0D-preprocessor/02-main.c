61d57ed62ad4e333c9110fcaaf4a1283dbd24938
elyton250 <nelysemathieu@gmial.com>
2023-08-14 16:25:47 +0200

    2-main bug

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
+  *main - Entry point of the program
   *
+  *Description: it prints the name of the source file
   *
-  *
-  *
-  *
+  *Return: 0 for sucess
   */
-int main (void)
+int main(void)
 {
 	printf("%s'\n'", __FILE__);
 	return (0);
