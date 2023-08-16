98846eb17492e60e4c4a4d2a58e4cfabd829376e
elyton250 <nelysemathieu@gmial.com>
2023-08-16 22:27:41 +0200

    k

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,10 @@
 #include "function_pointers.h"
-#include <stdlib.h>
 #include <stdio.h>
 /**
   *print_name - prints a string
   *@name: the string to be printed
   *@f: a function to be applied
+  *Return: void
   */
 void print_name(char *name, void (*f)(char *))
 {
