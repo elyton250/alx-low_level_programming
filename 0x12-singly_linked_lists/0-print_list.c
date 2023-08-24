ad6b1d34144389a1385efe403f1e9e62f35f5ee3
elyton250 <nelysemathieu@gmial.com>
2023-08-24 22:34:12 +0200

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,11 +14,11 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str == NULL)
 		{
-			printf("[%d]%s", 0, "(nil)");
+			printf("[%d]%s\n", 0, "(nil)");
 		}
 		else
 		{
-			printf("[%d]%s", h->len, h->str);
+			printf("[%d]%s\n", h->len, h->str);
 		}
 		h = h->next;
 		node++;
