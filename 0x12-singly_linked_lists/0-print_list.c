5cea8a03e00f5fdf515cb4afe8f268e367b85698
elyton250 <nelysemathieu@gmial.com>
2023-08-24 23:18:13 +0200

    add tasks

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,15 +10,15 @@ size_t print_list(const list_t *h)
 {
 	size_t node = 0;
 
-	while (h != NULL)
+	while (h)
 	{
-		if (h->str == NULL)
+		if (!h->str)
 		{
-			printf("[%d]%s\n", 0, "(nil)");
+			printf("[0] (nil)\n");
 		}
 		else
 		{
-			printf("[%d]%s\n", h->len, h->str);
+			printf("[%u]%s\n", h->len, h->str);
 		}
 		h = h->next;
 		node++;
