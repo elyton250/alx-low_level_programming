01b7451e383313f907c835c2440e6c49a2ad0047
elyton250 <nelysemathieu@gmal.com>
2023-10-12 22:19:53 +0200

    second tasks

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,15 +1,20 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include"lists.h"
+/**
+  *sum_dlistint - add data
+  *@head: the head of the list
+  *Return: the sum
+  */
 int sum_dlistint(dlistint_t *head)
 {
 	dlistint_t *temp = head;
 	int sum = 0;
 
-	while(temp != NULL)
+	while (temp != NULL)
 	{
 		sum += temp->n;
 		temp = temp->next;
 	}
-	return sum;
+	return (sum);
 }
--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,35 +1,42 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include"lists.h"
+/**
+  *insert_dnodeint_at_index - insert node at a posintion
+  *@h: the head of the list
+  *@idx: the index to remove it
+  *@n: the data
+  *Return: the node
+  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *temp = *h;
 	dlistint_t *newnode = malloc(sizeof(dlistint_t));
 	unsigned int i;
 
-	if(newnode == NULL)
+	if (newnode == NULL)
 		return (NULL);
 	newnode->n = n;
 
-	if(idx == 0)
+	if (idx == 0)
 	{
 		newnode->next = temp;
 		newnode->prev = NULL;
-		if (temp !=NULL)
+		if (temp != NULL)
 		{
 			temp->prev = newnode;
 		}
 		*h = newnode;
 		return (newnode);
 	}
-	for(i = 0; i < idx - 1 && temp != NULL; i++)
+	for (i = 0; i < idx - 1 && temp != NULL; i++)
 	{
 		temp = temp->next;
 	}
 	if (temp == NULL)
 	{
 		free(newnode);
-		return(NULL);
+		return (NULL);
 	}
 	newnode->next = temp->next;
 	newnode->prev = temp;
